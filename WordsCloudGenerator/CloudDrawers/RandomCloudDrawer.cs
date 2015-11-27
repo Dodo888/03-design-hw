d535d379b6bbeb13a986bd0a4181c4f9a5d3676d
Dodo888 <darkdodo888@gmail.com>
2015-11-27 15:20:35 +0500

    Small bugfixes
    -MaxTriesAmount is now constant
    -count is now fontSizeDecrease

--- WordsCloudGenerator/CloudDrawers/RandomCloudDrawer.cs
+++ WordsCloudGenerator/CloudDrawers/RandomCloudDrawer.cs
@@ -7,24 +7,26 @@ namespace WordsCloudGenerator.CloudDrawers
 {
     public class RandomCloudDrawer : ICloudDrawer
     {
+        private const int MaxTriesAmount = 100;
+
         public Bitmap FormCloud(Bitmap bitmap, Configuration config, List<string> words)
         {
             using (var graphics = Graphics.FromImage(bitmap))
             {
                 var random = new Random();
-                var count = 0;
+                var fontSizeDecrease = 0;
                 var occupiedAreas = new HashSet<RectangleF>();
                 graphics.Clear(ColorTranslator.FromHtml(config.BackgroundColor));
                 foreach (var word in words)
                 {
-                    var font = new Font(config.Font, Math.Max(config.MaxFontSize - 3*count, config.MinFontSize));
+                    var font = new Font(config.Font, Math.Max(config.MaxFontSize - 3*fontSizeDecrease, config.MinFontSize));
                     SizeF textSize = graphics.MeasureString(word, font);
                     Point textPlace = DefineArea(occupiedAreas, textSize, new Point(config.Width, config.Height));
                     graphics.DrawString(word, font,
                         new SolidBrush(ColorTranslator.FromHtml(config.Colors[random.Next(config.Colors.Count)])),
                         textPlace);
                     occupiedAreas.Add(new RectangleF(textPlace, textSize));
-                    count++;
+                    fontSizeDecrease++;
                 }
             }
             return bitmap;
@@ -39,7 +41,7 @@ namespace WordsCloudGenerator.CloudDrawers
             var triesAmount = 0;
             while (!hasStartPoint || HasIntersection(occupiedAreas, width, height, textSize))
             {
-                if (triesAmount > 100)
+                if (triesAmount > MaxTriesAmount)
                     break;
                 width = random.Next(bitmap.X - (int)textSize.Width);
                 height = random.Next(bitmap.Y - (int)textSize.Height);
